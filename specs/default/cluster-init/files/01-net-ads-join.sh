e51ddc7ad3fc80df737af40b266ed244052c1181
cc4bt <markus.szulta@bertrandt.com>
2024-07-26 15:13:35 +0200

    Update 01-net-ads-join.sh

--- specs/default/cluster-init/files/01-net-ads-join.sh
+++ specs/default/cluster-init/files/01-net-ads-join.sh
@@ -7,6 +7,6 @@ AD_OU=$(jetpack config adjoin.ad_ou)
 AD_DOMAIN=$(jetpack config adjoin.ad_domain)
 
 echo -e $AD_DOMAIN_NAME_SERVER > /etc/resolv.conf
-echo $AD_USER_PASSWORD | net ads join -U $AD_USER_NAME createcomputer=$AD_OU createupn=host/$HOSTNAME@$AD_DOMAIN
+echo $AD_USER_PASSWORD | net ads join -U $AD_USER_NAME createcomputer="$AD_OU" createupn=host/$HOSTNAME@$AD_DOMAIN
 authconfig --enablemkhomedir --update
 systemctl restart sssd
